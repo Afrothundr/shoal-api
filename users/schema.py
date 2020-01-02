6932e0741d8bc5d82ac0c1e4063a70fdf278c7a9
Branford Harris <branford.harris@intouchsol.com>
2020-01-02 11:34:13 -0600

    implemented add and remove friend functionality

--- users/schema.py
+++ users/schema.py
@@ -1,12 +1,8 @@
 import graphene
 from django.contrib.auth import get_user_model
 from graphene_django import DjangoObjectType
-
-
-class UserType(DjangoObjectType):
-    class Meta:
-        model = get_user_model()
-
+from .mutations import CreateUser, SaveUser, AddFriend, RemoveFriend
+from .types import UserType
 
 class Query(graphene.ObjectType):
     users = graphene.List(UserType)
@@ -22,50 +18,8 @@ class Query(graphene.ObjectType):
         return get_user_model().objects.all()
 
 
-class CreateUser(graphene.Mutation):
-    user = graphene.Field(UserType)
-
-    class Arguments:
-        username = graphene.String(required=True)
-        password = graphene.String(required=True)
-        firstName = graphene.String(required=True)
-        lastName = graphene.String(required=True)
-        email = graphene.String(required=True)
-
-    def mutate(self, info, username, firstName, lastName, password, email):
-        user = get_user_model()(
-            username=username,
-            email=email,
-            first_name=firstName,
-            last_name=lastName
-        )
-        user.set_password(password)
-        user.save()
-
-        return CreateUser(user=user)
-
-class SaveUser(graphene.Mutation):
-    user = graphene.Field(UserType)
-
-    class Arguments:
-        firstName = graphene.String(required=True)
-        lastName = graphene.String(required=True)
-        bio = graphene.String(required=True)
-
-    def mutate(self, info, firstName, lastName, bio):
-        user = info.context.user
-        if user.is_anonymous:
-            raise Exception('Not logged in!')
-
-        user.first_name = firstName
-        user.last_name = lastName
-        user.profile.bio = bio
-
-        user.save()
-
-        return SaveUser(user=user)
-
-
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
     save_user = SaveUser.Field()
+    add_friend = AddFriend.Field()
+    remove_friend = RemoveFriend.Field()
