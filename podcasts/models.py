fcabf3344b64b4825576d211c9f5578d16f018b9
Branford Harris <branford.harris@intouchsol.com>
2020-01-17 10:45:26 -0600

    Added creating and removing comments and likes on podcasts on comments
    - broke up api calls into utilities class
    - fixed comment mutations
    - added liking and unliking fuctionality
    - added ability to delete comments
    
    SHOAL-19, SHOAL-20

--- podcasts/models.py
+++ podcasts/models.py
@@ -13,3 +13,8 @@ class Comment(models.Model):
     body = models.TextField(max_length=240)
     date = models.DateTimeField(default=datetime.now, blank=True)
     posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+
+class Like(models.Model):
+    podcast = models.ForeignKey('podcasts.Podcast', related_name='likes', on_delete=models.DO_NOTHING, default=None, null=True)
+    comment = models.ForeignKey('podcasts.Comment', related_name='likes', on_delete=models.DO_NOTHING, default=None, null=True)
+    posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
