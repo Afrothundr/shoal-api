5d6c886e0def40537712e6e943fe8af30bf7fb99
Branford <branford.harris@outlook.com>
2020-12-20 23:33:11 -0600

    adding submodule

--- users/schema.py
+++ users/schema.py
@@ -1,5 +1,5 @@
 import graphene
-import pocketcasts
+from packages.pocketcasts import pocketcasts
 from django.http import JsonResponse
 from django.contrib.auth import get_user_model
 from graphene_django import DjangoObjectType
